ed3c03b3d934b5f9155b9dcd25c8ccb96295da0a
Yegor Bugayenko <yegor256@gmail.com>
2025-05-02 12:35:50 +0300

    #1 copyright fix

--- lib/joined.rb
+++ lib/joined.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2025 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 # Joins elements of array.
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
+# Copyright:: Copyright (c) 2025 Yegor Bugayenko
 # License:: MIT
 class Array
   def joined
--- test/test_joined.rb
+++ test/test_joined.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2025 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require_relative '../lib/joined'
 
 # Test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
+# Copyright:: Copyright (c) 2025 Yegor Bugayenko
 # License:: MIT
 class Testjoined < Minitest::Test
   def test_simple
