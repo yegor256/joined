432f56793fa7bfe65157c6780d4fddb5d02a0497
Yegor Bugayenko <yegor256@gmail.com>
2025-05-02 13:06:24 +0300

    #3 doc

--- lib/joined.rb
+++ lib/joined.rb
@@ -13,7 +13,7 @@ class Array
   # and placing "AND" between the last two items.
   #
   # @param [Boolean] oxford Should we place a comma before AND?
-  # @return [String] The text generated
+  # @return [String] The text generated (with items joined)
   def joined(oxford: true)
     return '' if empty?
     return first if length == 1
