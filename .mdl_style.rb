8384f6a51ca14b8c67b531ef583f69ee53949b37
Paul Gallagher <gallagher.paul@gmail.com>
2025-05-19 21:19:10 +0800

    add copyright to mdl style file

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# SPDX-FileCopyrightText: Copyright (c) 2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
 all
 # see rule documentation and defaults: https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md
 # e.g. to override line length check:
